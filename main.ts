ef614dc33b23db54d5044a7a6bf96c6135765152
elRadish <andreas.rettig@artcom.de>
2022-12-22 16:28:39 +0100

    Unbounce

--- main.ts
+++ main.ts
@@ -5,14 +5,16 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(eigeneNummer)
 })
 pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
-    radio.sendString("" + (counter))
+    radio.sendString("phone/onDialed " + counter)
     led.unplot(4, 0)
     basic.showNumber(counter)
 })
 pins.onPulsed(DigitalPin.P1, PulseValue.High, function () {
+    radio.sendString("phone/onStartDial")
     basic.clearScreen()
     counter = 0
     led.plot(4, 0)
+    control.waitMicros(2000)
 })
 pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
     led.unplot(0, 0)
@@ -24,20 +26,26 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(eigeneNummer)
 })
 pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
-    led.plot(0, 0)
-    if (counter < 9) {
-        counter += 1
-    } else {
-        counter = 0
+    if (control.millis() - lastPulse > 70) {
+        radio.sendString("phone/onPulse")
+        led.plot(0, 0)
+        if (counter < 9) {
+            counter += 1
+        } else {
+            counter = 0
+        }
+        lastPulse = control.millis()
     }
 })
+let lastPulse = 0
 let counter = 0
 let eigeneNummer = 0
+radio.setTransmitPower(7)
 radio.setGroup(1)
 eigeneNummer = 0
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 basic.showNumber(counter)
 basic.forever(function () {
-    serial.writeLine("" + pins.digitalReadPin(DigitalPin.P1) + " " + pins.digitalReadPin(DigitalPin.P0))
+	
 })
